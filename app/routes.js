2a051675d7ae1879a17ccb7a335e0ffdf3edbbf0
Paul Hayes <fofr@users.noreply.github.com>
2021-10-29 17:10:32 +0100

    Add initial example

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const data = require('../data/open-establishments.json')
 
 // Example wizard
 require('./routes/example-wizard')(router)
 
 // Add your routes here - above the module.exports line
+router.get('/api/v1/schools/', (req, res) => {
+  const query = req.query.search
+  const results = data.filter(({ name }) => name.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+  res.end(JSON.stringify(results.slice(0, 100)))
+})
 
 module.exports = router
