e60ca9127d8897ed9f584e21bc4ed61a6bdf82d3
Paul Hayes <fofr@users.noreply.github.com>
2021-11-01 13:08:09 +0000

    WIP

--- app/routes.js
+++ app/routes.js
@@ -8,8 +8,20 @@ require('./routes/example-wizard')(router)
 // Add your routes here - above the module.exports line
 router.get('/api/v1/schools/', (req, res) => {
   const query = req.query.search
-  const results = data.filter(({ name }) => name.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+  const results = data.filter(({ name }) => normaliseName(name).indexOf(normaliseName(query).toLowerCase()) !== -1)
   res.end(JSON.stringify(results.slice(0, 100)))
 })
+
+const normaliseName = (name) => {
+  let normalisedName = name.toLowerCase()
+
+  // remove punctuation
+  normalisedName = normalisedName.replace(/['.,/#!$%^&*;:{}=\-_`~()]/g, '')
+
+  // standardise church of england
+  // normalisedName = normalisedName.replace(/church of england/g, 'cofe')
+
+  return normalisedName
+}
 
 module.exports = router
